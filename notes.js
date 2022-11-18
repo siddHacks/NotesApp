6d7b4e7a91a7a186d8daa3ddd746809c65e5d2b8
siddHacks <siddtheprofessional@gmail.com>
2022-11-18 09:44:57 +0530

    popup box closing property add

--- notes.js
+++ notes.js
@@ -1,7 +1,18 @@
 const addBox=document.querySelector(".add-box"),
-popupBox=document.querySelector(".popup-box");
+popupBox=document.querySelector(".popup-box"),
+closeIcon=document.querySelector("header i"),
+addBtn=document.querySelector("button");
 
 
 addBox.addEventListener("click", ()=>{
     popupBox.classList.add("show");
+})
+
+closeIcon.addEventListener("click", ()=>{
+    popupBox.classList.remove("show");
+})
+
+addBtn.addEventListener("click", e=>{
+    e.preventDefault();
+    console.log("Button clicked")
 })
